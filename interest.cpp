736bbbf3771e584ffde2194f9db66419125b4cc9
sam.daniel <billbrat371@gmail.com>
2025-12-05 18:35:07 +0300

    Update interest.cpp

--- interest.cpp
+++ interest.cpp
@@ -1,3 +1,6 @@
+//samuel daniel
+//reg:BCS-03-0118/2025
+
 #include<iostream>
 using namespace  std;
 float sim_interest(float p,float r,float t){
@@ -8,4 +11,5 @@ int main(){
     s_interest=sim_interest(20000,10,12);
     cout <<"Your interest is :"<<s_interest<<endl;
     return 0;
+
 }
