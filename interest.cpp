477e311898df62a1b3dd477b9b47c3c4882712ea
sam.daniel <billbrat371@gmail.com>
2025-12-05 17:21:58 +0300

    Add files via upload

---
+++ interest.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace  std;
+float sim_interest(float p,float r,float t){
+    return p*(r/100)*t;
+}
+int main(){
+    float p,r,t,s_interest;
+    s_interest=sim_interest(20000,10,12);
+    cout <<"Your interest is :"<<s_interest<<endl;
+    return 0;
+}
